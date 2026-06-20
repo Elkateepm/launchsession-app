45393e17a41e431634a058d614037a14ea8f58a7
Mohammed Elkateep <Mohammed.elkateep@outlook.com>
2026-06-21 00:13:52 +0100

    hardcode supabase url/anon key to remove env var dependency

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
-)
+
+const SUPABASE_URL = 'https://ssahcqeqrxawmwtjpwvh.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzYWhjcWVxcnhhd213dGpwd3ZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNDYyNTEsImV4cCI6MjA5MzcyMjI1MX0.HYCzuHe5C1cWoxh7yYUZuLWG0bxvy_9xTE1bmlwJweQ'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
